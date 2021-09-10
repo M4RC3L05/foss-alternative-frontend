af926c6c7c8d9643bc19e9f520da6e5e7c0eb393
m4rc3l05 <joaobat@live.com.pt>
2021-09-10 21:07:52 +0100

    Cleanup ui component code
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

--- src/ui/proprietary-search/proprietary-search.controller.ts
+++ src/ui/proprietary-search/proprietary-search.controller.ts
@@ -2,27 +2,17 @@ import { searchPropriataryUseCase } from "../../use-cases/search-proprietary.use
 import { debounce } from "../../utils/functions";
 import { TState } from "../state";
 
-const onSearch = async (state: TState, txt: string) => {
-  const { data, error } = await searchPropriataryUseCase({ term: txt });
-
-  if (!data || error) {
-    state.results = [];
-    return;
-  }
-
-  state.results = [...data];
-};
-
-const doSearchDebounce = debounce(onSearch, 250);
-
 export const onInputChange = (state: TState) => (event: Event) => {
   const txt = (event.target as HTMLInputElement).value;
 
   state.searchQuery = txt;
 };
 
-export const onTerm = (state: TState) => (term: string) => {
-  if (term) {
-    void doSearchDebounce(state, term);
-  }
-};
+export const onTerm = (state: TState) =>
+  debounce(async (term: string) => {
+    if (term) {
+      const { data, error } = await searchPropriataryUseCase({ term });
+
+      state.results = !data || error ? [] : data;
+    }
+  }, 250);
