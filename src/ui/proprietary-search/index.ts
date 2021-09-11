656d690733346c69b7745245584949a3f13f5d6a
m4rc3l05 <joaobat@live.com.pt>
2021-09-11 12:35:26 +0100

    Refactor proprietary search code
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

--- src/ui/proprietary-search/index.ts
+++ src/ui/proprietary-search/index.ts
@@ -1,10 +1,34 @@
+import { TSoftware } from "../../domain/software/software";
 import { onState, state } from "../state";
-import { onInputChange, onTerm } from "./proprietary-search.controller";
+import {
+  disableIsSearching,
+  onInputChange,
+  onInputFocus,
+  onTerm,
+  onCloseSearchResult,
+  onSetInputValueToSearchQuery,
+  onSetSearchValueToSeelectedResultName,
+} from "./proprietary-search.controller";
 import { searchInput } from "./proprietay-search.ui";
 
 const onInputChangeBinded = onInputChange(state);
 const onTermBinded = onTerm(state);
+const disableIsSearchingBinded = disableIsSearching(state);
+const onInputFocusBinded = onInputFocus(state);
+const onCloseSearchResultBinded = onCloseSearchResult(state);
+const onSetInputValueToSearchQueryBinded = onSetInputValueToSearchQuery(state);
 
 searchInput.addEventListener("input", onInputChangeBinded);
-
+searchInput.addEventListener("focusin", onInputFocusBinded);
+window.addEventListener("click", onCloseSearchResultBinded);
+searchInput.addEventListener("focusin", onSetInputValueToSearchQueryBinded);
 onState<string>("searchQuery", onTermBinded);
+onState<string>("searchQuery", disableIsSearchingBinded);
+onState<{
+  searchQuery: string;
+  selectedProprietary: TSoftware | null;
+  isSearching: boolean;
+}>(
+  ["selectedProprietary", "searchQuery", "isSearching"],
+  onSetSearchValueToSeelectedResultName
+);
