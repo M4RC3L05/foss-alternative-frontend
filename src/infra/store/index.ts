8bfb2adde1f6e86c2f68567c230c30230afd84a6
m4rc3l05 <joaobat@live.com.pt>
2021-09-11 12:37:40 +0100

    Refactor store
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

--- src/infra/store/index.ts
+++ src/infra/store/index.ts
@@ -1,3 +1,6 @@
+import get from "lodash.get";
+import pick from "lodash.pick";
+
 export type TListener<T> = (data: T) => void;
 
 const handler = <T extends Record<string, unknown>>(
@@ -24,7 +27,13 @@ const handler = <T extends Record<string, unknown>>(
       throw new Error(`"${key as string}" do not exists on object`);
     }
 
-    if (typeof target[key as string] === "object") {
+    if (
+      typeof target[key as string] === "object" &&
+      !Array.isArray(target[key as string]) &&
+      target[key as string] !== null &&
+      target[key as string] !== undefined &&
+      typeof target[key as string] !== "symbol"
+    ) {
       return new Proxy(
         target[key as string] as Record<string, unknown>,
         handler(`${[previousKey, key].filter((x) => x).join(".")}`, onSet)
@@ -35,11 +44,19 @@ const handler = <T extends Record<string, unknown>>(
   },
 });
 
+const isString = (x: any): x is string => typeof x === "string";
+
 export const store = <T extends Record<string, unknown>>(initial: T) => {
   const __state: T = initial;
   const listeners = new Map<string, Array<TListener<any>>>();
 
-  const onState = <T>(stateKey: string, fn: TListener<T>) => {
+  const onState = <T>(stateKey: string | string[], fn: TListener<T>) => {
+    if (isString(stateKey)) {
+      stateKey = [stateKey];
+    }
+
+    stateKey = stateKey.toString();
+
     if (!listeners.has(stateKey)) {
       listeners.set(stateKey, []);
     }
@@ -50,16 +67,43 @@ export const store = <T extends Record<string, unknown>>(initial: T) => {
   const state = new Proxy<T>(
     __state,
     handler("", (key, value, previous) => {
-      if (previous !== value && listeners.has(key)) {
-        for (const listener of listeners.get(key)!) {
-          listener(value);
+      const listenersKeys = [...listeners.keys()];
+      const matchingKeys = listenersKeys.filter((lkey) =>
+        lkey.split(",").includes(key)
+      );
+      if (previous !== value && matchingKeys.length > 0) {
+        for (const lkey of matchingKeys) {
+          for (const listener of listeners.get(lkey)!) {
+            listener(pickState(lkey));
+          }
         }
       }
     })
   );
 
+  const pickState = (key: string) => {
+    const keys = key.split(",");
+
+    if (keys.length === 1) {
+      return get(__state, key) as unknown;
+    }
+
+    return pick(__state, keys);
+  };
+
+  const notifyAll = () => {
+    for (const [key, subs] of listeners.entries()) {
+      const state = pickState(key);
+
+      for (const sub of subs) {
+        sub(state);
+      }
+    }
+  };
+
   return {
     state,
     onState,
+    notifyAll,
   };
 };
