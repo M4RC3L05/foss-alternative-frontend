62569998032e2946495a09e04ee8cb25b7fadb45
m4rc3l05 <joaobat@live.com.pt>
2021-09-11 12:36:13 +0100

    Refactor and add styles to proprietary display
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

--- src/ui/proprietary-display/proprietary-display.ui.ts
+++ src/ui/proprietary-display/proprietary-display.ui.ts
@@ -1,29 +1,84 @@
 import { html, render } from "lit-html";
 import { TSoftware } from "../../domain/software/software";
+import styles from "./proprietary-display.module.css";
 
-const result = (
-  software: TSoftware,
-  onItemClick: (item: TSoftware) => void
-) => html`
+export const displayProprietaryEle = document.querySelector<HTMLDivElement>(
+  "#proprietary-display"
+)!;
+
+const searchResult = ({
+  software,
+  onItemClick,
+}: {
+  software: TSoftware;
+  onItemClick: (item: TSoftware) => void;
+}) => html`
   <div
+    .className=${styles.search__results__item}
     @click=${() => {
       onItemClick(software);
     }}
   >
     <img .src=${software.iconLink!} .width=${50} .height=${60} />
-    <p>${software.name}</p>
+    <div .className=${styles.search__results__item__info}>
+      <p>${software.name}</p>
+    </div>
   </div>
 `;
 
-const results = (items: TSoftware[], onItemClick: (item: TSoftware) => void) =>
-  html`${items.map((item) => result(item, onItemClick))}`;
+const searchResults = ({
+  results,
+  isSearching,
+  searchQuery,
+  onItemClick,
+  isPerformingSearch,
+}: {
+  results: TSoftware[];
+  isSearching: boolean;
+  searchQuery: string;
+  isPerformingSearch: boolean;
+  onItemClick: (item: TSoftware) => void;
+}) =>
+  html`
+    ${searchQuery.length <= 0 &&
+    isSearching &&
+    !isPerformingSearch &&
+    results.length <= 0
+      ? html`<span style="color: #949494; margin-left: 20px"
+          >Search results will appear here.</span
+        >`
+      : isSearching && !isPerformingSearch && results.length <= 0
+      ? html`<span style="color: #949494; margin-left: 20px"
+          >No results found.</span
+        >`
+      : isSearching && isPerformingSearch
+      ? html`<span style="color: #949494; margin-left: 20px"
+          >Searching...</span
+        >`
+      : results.map((item) => searchResult({ software: item, onItemClick }))}
+  `;
 
-export const renderResults = (
-  items: TSoftware[],
-  onItemClick: (item: TSoftware) => void
-) => {
+export const renderResults = ({
+  results,
+  isPerformingSearch,
+  isSearching,
+  searchQuery,
+  onItemClick,
+}: {
+  results: TSoftware[];
+  isSearching: boolean;
+  isPerformingSearch: boolean;
+  searchQuery: string;
+  onItemClick: (item: TSoftware) => void;
+}) => {
   render(
-    results(items, onItemClick),
-    document.querySelector<HTMLDivElement>("#proprietary-display")!
+    searchResults({
+      results,
+      isSearching,
+      searchQuery,
+      isPerformingSearch,
+      onItemClick,
+    }),
+    displayProprietaryEle
   );
 };
